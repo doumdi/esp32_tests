9df66f3a54a5ab79792510a0efc94ad8fcb028c7
Dominic Létourneau <doumdi@gmail.com>
2017-12-14 15:48:36 -0500

    imu working, basic

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <Adafruit_GPS.h>
 #include <WiFi.h>
 #include <HardwareSerial.h>
+#include <SparkFunMPU9250-DMP.h>
+
+
+//Prototypes
+void printIMUData(void);
+
 
 //UART 2
 //rxPin = 16;
@@ -11,6 +17,7 @@ HardwareSerial GPSSerial(2);
 // Connect to the GPS on the hardware port
 Adafruit_GPS GPS(&GPSSerial);
 
+MPU9250_DMP imu;
 
 
 hw_timer_t * timer = NULL;
@@ -36,7 +43,7 @@ void setup() {
     Serial.begin(115200);
 
     delay(1000);
-    Serial.println("Adafruit GPS logging data dump!");
+    Serial.println("\nAdafruit GPS logging data dump!");
 
     // 9600 NMEA is the default baud rate for MTK - some use 4800
     GPS.begin(9600);
@@ -73,15 +80,50 @@ void setup() {
     timerAlarmWrite(timer, 1000000, true);
 
     // Start an alarm
-    timerAlarmEnable(timer);
+    //timerAlarmEnable(timer);
 
+    //Initialize imu
+    if (imu.begin() == INV_SUCCESS)
+    {
+      Serial.println("MPU9250 initialized");
+
+      // Enable all sensors:
+      imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+      // Use setGyroFSR() and setAccelFSR() to configure the
+      // gyroscope and accelerometer full scale ranges.
+      // Gyro options are +/- 250, 500, 1000, or 2000 dps
+      imu.setGyroFSR(2000); // Set gyro to 2000 dps
+
+      // Accel options are +/- 2, 4, 8, or 16 g
+      imu.setAccelFSR(2); // Set accel to +/-2g
+      // Note: the MPU-9250's magnetometer FSR is set at
+      // +/- 4912 uT (micro-tesla's)
+
+      // setLPF() can be used to set the digital low-pass filter
+      // of the accelerometer and gyroscope.
+      // Can be any of the following: 188, 98, 42, 20, 10, 5
+      // (values are in Hz).
+      imu.setLPF(5); // Set LPF corner frequency to 5Hz
+
+
+      // The sample rate of the accel/gyro can be set using
+      // setSampleRate. Acceptable values range from 4Hz to 1kHz
+      imu.setSampleRate(10); // Set sample rate to 10Hz
+
+      // Likewise, the compass (magnetometer) sample rate can be
+      // set using the setCompassSampleRate() function.
+      // This value can range between: 1-100Hz
+      imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+
+    }
 
 }
 
 
 
-
-void loop() {
+void loop()
+{
 
   while (GPSSerial.available()) {
     char c = GPS.read();
@@ -131,5 +173,50 @@ void loop() {
   }//while
 } // if timer_ready
 
+// dataReady() checks to see if new accel/gyro data
+// is available. It will return a boolean true or false
+// (New magnetometer data cannot be checked, as the library
+//  runs that sensor in single-conversion mode.)
+if ( imu.dataReady() )
+{
+  // Call update() to update the imu objects sensor data.
+  // You can specify which sensors to update by combining
+  // UPDATE_ACCEL, UPDATE_GYRO, UPDATE_COMPASS, and/or
+  // UPDATE_TEMPERATURE.
+  // (The update function defaults to accel, gyro, compass,
+  //  so you don't have to specify these values.)
+  imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+  printIMUData();
+} //imu.dataReady
 
 } //loop
+
+
+void printIMUData(void)
+{
+  // After calling update() the ax, ay, az, gx, gy, gz, mx,
+  // my, mz, time, and/or temerature class variables are all
+  // updated. Access them by placing the object. in front:
+
+  // Use the calcAccel, calcGyro, and calcMag functions to
+  // convert the raw sensor readings (signed 16-bit values)
+  // to their respective units.
+  float accelX = imu.calcAccel(imu.ax);
+  float accelY = imu.calcAccel(imu.ay);
+  float accelZ = imu.calcAccel(imu.az);
+  float gyroX = imu.calcGyro(imu.gx);
+  float gyroY = imu.calcGyro(imu.gy);
+  float gyroZ = imu.calcGyro(imu.gz);
+  float magX = imu.calcMag(imu.mx);
+  float magY = imu.calcMag(imu.my);
+  float magZ = imu.calcMag(imu.mz);
+
+  Serial.println("Accel: " + String(accelX) + ", " +
+              String(accelY) + ", " + String(accelZ) + " g");
+  Serial.println("Gyro: " + String(gyroX) + ", " +
+              String(gyroY) + ", " + String(gyroZ) + " dps");
+  Serial.println("Mag: " + String(magX) + ", " +
+              String(magY) + ", " + String(magZ) + " uT");
+  Serial.println("Time: " + String(imu.time) + " ms");
+  Serial.println();
+}
